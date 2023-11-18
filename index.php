fc7c641ae64852524c379ee625b3c872a9cdc8a6
programstdent <151394680+programstdent@users.noreply.github.com>
2023-11-19 00:45:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>โรงเรียนมัธยมศึกษาจุฬาภรณ์</title>
+
+        <link rel="stylesheet" href="bootstrap.min.css" crossorigin="anonymous">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-secondary">
+            <div class="container">
+                <a class="navbar-brand" href="#"> <img src="logoschool.jpg" width="80px" height="" border="0" alt="">   โรงเรียนมัธยมศึกษาจุฬาภรณ์    </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.php">หน้าแรก<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="reportstudent.php">รายงานนักเรียน</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="student.php">นักเรียน</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="teacher.php">ครู</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="director.php">ผู้อำนวยการโรงเรียน</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="registrar.php">นายทะเบียน</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="admin.php">ผู้ดูแลระบบ</a>
+                        </li>
+                    </ul>
+      
+	            </div>
+            </div>
+        </nav>
+
+
+        <br><br><br><br><br><br>
+
+
+
+        <div class="container " >
+            <div class="row ">
+                <div class="col-4 p-2 mb-2 shadow-lg  " align="right" style="background-color:#fe7200;"><strong><font size="" color="#fffff">
+ 
+                    <div class="d-lg-none"> <font size="2" color=""><strong> Breaking News</strong></font></div>
+                    <div class="d-none d-lg-block"> <strong>MJP Breaking News</strong></div>
+ 
+                    </font></strong> </div>
+                    <div class="col-8 p-2 mb-2  text-dark shadow-lg  "> <marquee class="news-scroll" behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();"> 
+                        <a href=''><strong>ยินดีต้อนรับเข้าสู่เว็บไซต์โรงเรียนมัธยมศึกษาจุฬาภรณ์</a> <span class='dot'></span>  
+
+                        </marquee> 
+                    </div>
+
+
+
+
+				</div>
+			</div>
+        </div>
+
+
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100 " src="jp1.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="jp2.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="jp3.jpg" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+        <br><br><br><br>
+
+        <center>
+            <img src="footer.jpg" width="50%" hieght="25%">
+        </center>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="jquery-3.2.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="popper.min.js"  crossorigin="anonymous"></script>
+        <script src="bootstrap.min.js"  crossorigin="anonymous"></script>
+    </body>
+</html>
